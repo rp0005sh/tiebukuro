39fb7afa6eb09932ed2ae64f115c108c55de06b9
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2018-06-02 16:58:15 +0900

    Create q10191087815.java
    じゃんけんの勝敗判定メソッド

---
+++ q10191087815.java
@@ -0,0 +1,29 @@
+public static String judgeGame(String handA, String handB) {
+  String ret = "";
+  switch (handA) {
+    case "グー":
+      switch (handB) {
+        case "グー"  : ret = "引き分け";    break;
+        case "チョキ": ret = "Aさんの勝ち"; break;
+        case "パー"  : ret = "Bさんの勝ち"; break;
+      }
+      break;
+    case "チョキ":
+      switch (handB) {
+        case "グー"  : ret = "Bさんの勝ち"; break;
+        case "チョキ": ret = "引き分け";    break;
+        case "パー"  : ret = "Aさんの勝ち"; break;
+      }
+      break;
+    case "パー":
+      switch (handB) {
+        case "グー"  : ret = "Aさんの勝ち"; break;
+        case "チョキ": ret = "Bさんの勝ち"; break;
+        case "パー"  : ret = "引き分け";    break;
+      }
+      break;
+  }
+
+  if (ret.isEmpty()) new IllegalArgumentException("引数がおかしい(" + handA + "," + handB + ")");
+  return ret;
+}
