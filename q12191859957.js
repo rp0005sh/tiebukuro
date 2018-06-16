34905215a5dd3cbdb0439b97922ea2bab24a5819
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2018-06-16 23:26:27 +0900

    ボタンをClickで「寒」「暖」を表示する

---
+++ q12191859957.js
@@ -0,0 +1,12 @@
+<form>
+  <input type="button" value="実行" onclick="task()">
+  <input type="text" id="output" size="30">
+</form>
+<script>
+var arr = ["寒","寒寒","寒寒寒","暖","暖暖","暖暖暖","暖暖暖暖"];
+var count = 0;
+function task(){
+  var output = document.forms[0]["output"];
+  output.value = arr[(count++) % 7];
+}
+</script>
