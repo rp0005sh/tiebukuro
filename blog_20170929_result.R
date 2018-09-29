0aebc6d290e43a4c2d58ed1a99a21622994c0819
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2018-09-29 21:53:33 +0900

    降順ソート時間を評価(IntStream vs Stream<Integer>)
    降順ソート時間を評価(IntStream vs Stream<Integer>)

---
+++ blog_20170929_result.R
@@ -0,0 +1,18 @@
+> # IntStreamの速度
+> x1 =c(418,375,391,371,371,379,377,369,392,423)
+
+> # Stream<Integer>変換型の速度
+> x2 = c(2678,2603,2613,2593,2612,2593,2525,2553,2524,2563)
+
+> t.test(x1, x2, alternative="less")
+
+        Welch Two Sample t-test
+
+data:  y and x
+t = -137.93, df = 12.089, p-value < 2.2e-16
+alternative hypothesis: true difference in means is less than 0
+95 percent confidence interval:
+      -Inf -2170.701
+sample estimates:
+mean of x mean of y 
+    386.6    2585.7
