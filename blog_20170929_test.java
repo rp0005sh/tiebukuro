5731be216b134557ff450da2cbbf8a43f64ecf5c
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2018-09-29 21:12:54 +0900

    降順ソート時間を計測
    降順ソート時間を計測

---
+++ blog_20170929_test.java
@@ -0,0 +1,33 @@
+/** 
+ * Stream<Integer>に変換してComparatorを使って降順ソート
+ */
+class Main1 {
+    public static void main(String[] args) {
+        // 計測開始
+        long start = System.currentTimeMillis();
+
+        int[] arr = java.util.stream.IntStream.range(0, 50000000)
+            .boxed().sorted(java.util.Comparator.reverseOrder()).mapToInt(Integer::valueOf).toArray();
+
+        // 計測完了
+        long end = System.currentTimeMillis();
+        System.out.println("かかった時間:" + (end - start) + "[msec]");
+    }
+}
+
+/** 
+ * IntStreamのまま降順ソート
+ */
+class Main2 {
+    public static void main(String[] args) {
+        // 計測開始
+        long start = System.currentTimeMillis();
+
+        int[] arr = java.util.stream.IntStream.range(0, 50000000)
+            .map(n->-n).sorted().map(n -> -n).toArray();
+
+        // 計測完了
+        long end = System.currentTimeMillis();
+        System.out.println("かかった時間:" + (end - start) + "[msec]");
+    }
+}
