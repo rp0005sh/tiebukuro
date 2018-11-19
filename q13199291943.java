5ebb8793616db09e52d8d85a9f5de57258dd3f23
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2018-11-20 02:02:25 +0900

    乱数50個作って、合計値100を超えるタイミングをチェック
    乱数50個作って、合計値100を超えるタイミングをチェック

---
+++ q13199291943.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // 0から9の乱数を50個発生させ、配列にする
+        int[] rand = new java.util.Random().ints(50, 0, 10).toArray();
+
+        // 繰り返し文で乱数を一つずつ足していき、100を超えるところをチェック
+        int sum = 0, i;
+        for(i = 0; i < rand.length; i++) {
+            int tmp = sum + rand[i]; // 乱数を一つ足す
+            if (tmp > 100) break;    // 100を超えるかチェック。超えてたら終了
+            else sum = tmp;          // 100を超えていなければ、合計値を更新
+        }
+        System.out.println(i < rand.length
+            ? i + "番目で100を超える。100を超える前の合計は" + sum
+            : "全部足しても100を超えない");
+    }
+}
