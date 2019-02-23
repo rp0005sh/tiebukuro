0f2d0364a712386083c208bc3d6f7cb376726196
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2019-02-23 09:44:37 +0900

    High&Low簡易ゲーム
    High&Low簡易ゲーム(「負けるまで試合を続ける」を追加)

--- q13203799668.java
+++ q13203799668.java
@@ -3,28 +3,39 @@ public class Main {
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
         java.util.Random rand = new java.util.Random();
-        int num1 = rand.nextInt(13) + 1, num2 = rand.nextInt(13) + 1;
+        int num1, num2 = rand.nextInt(13) + 1;
 
-        System.out.println("High＆Low");
-        System.out.println(num1 + " ?");
+        // 勝ったら次も試合を続けるフラグ(試合に勝った、アイコだった場合にtrue、負けたらfalseにする
+        boolean next = true;
 
-        System.out.print("HかLを入力してください>");
-        String ans = sc.next();
+        while (next) {
+            // カードを更新
+            num1 = num2;
+            num2 = rand.nextInt(13) + 1;
+            
+            System.out.println("High＆Low");
+            System.out.println(num1 + " ?");
 
-        System.out.println("High＆Low");
-        System.out.println(num1 + " " + num2);
+            System.out.print("HかLを入力してください>");
+            String ans = sc.next();
 
-        switch(ans) {
-            case "L":
-                System.out.println("あなたはLowを選びました。");
-                System.out.println(num2 < num1 ? "You Win！" : "You Lose");
-                break;
-            case "H":
-                System.out.println("あなたはHighを選びました。");
-                System.out.println(num2 > num1 ? "You Win！" : "You Lose");
-                break;
-            default:
-                System.out.println("入力値不正。「H」か「L」以外が入力されました。");
+            System.out.println("High＆Low");
+            System.out.println(num1 + " " + num2);
+
+            switch(ans) {
+                case "L":
+                    System.out.println("あなたはLowを選びました。");
+                    System.out.println(num2 <= num1 ? "You Win！" : "You Lose");
+                    next = (num2 <= num1);
+                    break;
+                case "H":
+                    System.out.println("あなたはHighを選びました。");
+                    System.out.println(num2 >= num1 ? "You Win！" : "You Lose");
+                    next = (num2 >= num1);
+                    break;
+                default:
+                    System.out.println("入力値不正。「H」か「L」以外が入力されました。");
+            }
         }
     }
 }
