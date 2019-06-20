4fe3e5209c7c656715914ae2c835aeddfe48a824
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2019-06-20 09:55:05 +0900

    スクリプトエンジンでJsonパース
    スクリプトエンジンでJsonパース

--- q10209284410.java
+++ q10209284410.java
@@ -4,25 +4,19 @@ import javax.script.ScriptEngineManager;
 
 class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws javax.script.ScriptException {
 
         // 変換対象Jsonデータ
         String script = "{\"りんご\":\"apple\", \"みかん\":\"orange\"}";
         System.out.println("変換データ：" + script);
 
-        try {
-            // エンジン起動して、実行
-            ScriptEngineManager manager = new ScriptEngineManager();
-            ScriptEngine engine = manager.getEngineByName("nashorn");
-            Object obj = engine.eval("(" + script + ")");
-            Map<String, Object> map = (Map<String, Object>)obj;
+        // エンジン起動して、実行
+        ScriptEngineManager manager = new ScriptEngineManager();
+        ScriptEngine engine = manager.getEngineByName("nashorn");
+        Map<String, Object> map = (Map<String, Object>) engine.eval("(" + script + ")");
 
-            // map中身確認
-            System.out.println("りんご : " + map.get("りんご"));
-            System.out.println("みかん : " + map.get("みかん"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        // map中身確認
+        System.out.println("りんご : " + map.get("りんご"));
+        System.out.println("みかん : " + map.get("みかん"));
     }
 }
