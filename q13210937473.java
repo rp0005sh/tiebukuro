15d5e443496eadb1f04eb9b97e5bcd469759292d
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2019-07-30 10:32:08 +0900

    【宿題】最大値を求めるプログラム
    【宿題】最大値を求めるプログラム

---
+++ q13210937473.java
@@ -0,0 +1,36 @@
+class NumberControl {
+    /**
+     * 引数で渡された二つの整数を比較して大きいほうを戻す
+     * @param a 比較対象の整数
+     * @param b 比較対象の整数
+     * @return b 引数の大きい方の整数
+     */
+    static int selectMax(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    /**
+     * 引数が三つの場合でも対応
+     * @param a 比較対象の整数
+     * @param b 比較対象の整数
+     * @param c 比較対象の整数
+     * @return b 引数のうち一番大きい整数
+     */
+    static int selectMax(int a, int b, int c) {
+        return selectMax(selectMax(a, b), c);
+    }
+}
+
+class Run5 {
+    public static void main(String args[]) {
+        // 引数2個の場合の評価
+        System.out.println(NumberControl.selectMax(-10, 120));
+        System.out.println(NumberControl.selectMax(300, 150));
+
+        // 引数3個の場合の評価
+        System.out.println(NumberControl.selectMax(1, 1, 1));
+        System.out.println(NumberControl.selectMax(2, 1, 1));
+        System.out.println(NumberControl.selectMax(2, 3, 1));
+        System.out.println(NumberControl.selectMax(2, 3, 4));
+    }
+}
