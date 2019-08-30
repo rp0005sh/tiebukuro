522cdd880b2e5cadc4d1221069c6e09437218ba6
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2019-08-31 02:06:31 +0900

    行の要素間にスペースを入れる
    行の要素間にスペースを入れる

--- q13212757678.java
+++ q13212757678.java
@@ -1,22 +1,16 @@
 for (int i = 1; i <= h; i++) {
-
-    // 1行分の表示する文字列を保持する変数定義
-    String str = "";
-
     for (int j = 1; j <= w; j++) {
         int p = sc.nextInt();
         if (p >= 128) { // 128以上は白
-            str = str + "1"; // 変数に「１」を追加
+            System.out.print("1");
         } else if (p <= 127) { // 127以下は黒
-            str = str + "0"; // 変数に「0」を追加
+            System.out.print("0");
         }
-
-        // 要素間のスペースを入れる
-        if (j != w) { // 行の末尾は入れないので、j が wと一致するときは行わない
-            str = str + " ";
+        
+        // 行の末尾の数値でなければスペースを入れておく
+        if (j != w) {
+            System.out.print(" ");
         }
     }
-
-    // 一行分のデータを出力する
-    System.out.println(str);
+    System.out.println();
 }
