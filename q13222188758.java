71b87d2e5465c1ca2c1a61acff42f5844370a106
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2020-03-27 19:59:29 +0900

    月ごとの収支を表示させ、最後に合計値を表示
    月ごとの収支を表示させ、最後に合計値を表示

---
+++ q13222188758.java
@@ -0,0 +1,26 @@
+import java.util.stream.Stream;
+import java.util.Map;
+import java.time.Month;
+import static java.util.stream.Collectors.*;
+import static java.time.format.TextStyle.SHORT;
+import static java.util.Locale.US;
+
+class Main {
+    public static void main(String[] args) {
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+
+        // 月ごとの合計値を求める
+        final int n = sc.nextInt();
+        Map<Integer, Long> map = Stream.generate(() -> new int[] {sc.nextInt(), sc.nextInt(), sc.nextInt()})
+            .limit(n).collect(groupingBy(arr -> arr[1], summingLong(arr -> arr[0])));
+
+        // １月から順番に合計値を表示
+        map.entrySet().stream().sorted((e1, e2) -> e1.getKey() - e2.getKey())
+            .map(e -> Month.of(e.getKey()).getDisplayName(SHORT, US) + ":" + e.getValue())
+            .forEach(System.out::println);
+
+        // 総計値を表示する
+        System.out.printf("Total:%d%n",
+            map.values().stream().mapToLong(Long::longValue).sum());
+    }
+}
