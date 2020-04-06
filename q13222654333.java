0815e6869586815d8e81fe38f5a8ffe17bb88af2
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2020-04-07 00:05:12 +0900

    【宿題】四則演算のメソッドを車輪開発
    【宿題】四則演算のメソッドを車輪開発

--- q13222654333.java
+++ q13222654333.java
@@ -21,8 +21,8 @@ class Main {
 
     /**
      * 与えられた引数を利用して、足し算を行う
-     * @param base 足し算に使用する数字
-     * @param nums 足し算に使用する数字
+     * @param a 足し算に使用する数字
+     * @param b 足し算に使用する数字
      * @return 足し算の計算結果
      */
     static double calcPuls(double a, double b) {
@@ -31,8 +31,8 @@ class Main {
 
     /**
      * 与えられた引数を利用して、引き算を行う
-     * @param base 引き算に使用する数字
-     * @param nums 引き算に使用する数字
+     * @param a 引き算に使用する数字
+     * @param b 引き算に使用する数字
      * @return 引き算の計算結果
      */
     static double calcMinus(double a, double b) {
@@ -41,8 +41,8 @@ class Main {
 
     /**
      * 与えられた引数を利用して、かけ算を行う
-     * @param base かけ算に使用する数字
-     * @param nums かけ算に使用する数字
+     * @param a かけ算に使用する数字
+     * @param b かけ算に使用する数字
      * @return かけ算の計算結果
      */
     public static double calcMultiplication(double a, double b) {
@@ -51,8 +51,8 @@ class Main {
 
     /**
      * 与えられた引数を利用して、割算を行う
-     * @param base 割算に使用する数字
-     * @param nums 割算に使用する数字
+     * @param a 割算に使用する数字
+     * @param b 割算に使用する数字
      * @return 割算の計算結果
      */
     public static double calcDivision(double a, double b) {
