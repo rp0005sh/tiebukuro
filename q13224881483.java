9b6342b1297e9b8a61a00473f6031783b17d2834
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2020-05-14 02:20:18 +0900

    正方形に対角線を引く
    正方形に対角線を引く

---
+++ q13224881483.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+class Main {
+    public static void main(String[] args) {
+
+        // 0 ≦ x < 10, 0 ≦ y < 10の空間を作り、「.」で埋める
+        String[][] map = new String[10][10];
+        for (String[] col : map) Arrays.fill(row, "."); 
+
+      /* +----------------------------------------------------------+
+         | 二次元配列で空間を作る際は 配列[y座標][x座標]となります。| 
+         +----------------------------------------------------------+*/
+        // f(x) = xに該当する部分を"X"に塗り替える
+        for (int x = 0; x < 10; x++) map[x][x] = "X";
+        // f(x) = -x + 10 に該当する部分を"X"に塗り替える
+        for (int x = 0; x < 10; x++) map[-x + 10-1][x] = "X";
+
+
+        // 全体を表示する
+        for (String[] col : map) System.out.println(String.join("", row));
+    }
+}
