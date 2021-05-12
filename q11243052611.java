df3161761110158f888f889a92c3d53010a5afdf
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2021-05-13 07:02:15 +0900

    Listの数値を昇順ソート
    Listの数値を昇順ソート

---
+++ q11243052611.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import static java.util.Comparator.naturalOrder;
+
+class Main {
+  public static void main(String[] args) {
+    ArrayList<Integer> number = new ArrayList<>();
+    Scanner scan = new Scanner(System.in);
+    for (int i = 0;i < 10; i++) {
+      System.out.println("整数を入力してください");
+      int num = scan.nextInt();
+      number.add(num);
+    }
+  	number.sort(naturalOrder());
+    System.out.println(number);
+  }
+}
