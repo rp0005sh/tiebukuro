99e9f690509baf077abbf92d5be42117a222e338
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2022-08-21 16:14:53 +0900

    Update q10266710380.java

--- q10266710380.java
+++ q10266710380.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String[] str = sc.nextLine().split("(?<=^SET \\d)");
+        String[] str = sc.nextLine().split("(?<=^SET \\d)"); // 右記の配列に変換str = {"SET X", " 数値"}
         int a = 0, b = 0;
         switch(str[0]) {
             case "SET 1" : a = Integer.parseInt(str[1].substring(1)); break;
             case "SET 2" : b = Integer.parseInt(str[1].substring(1)); break;
             default: throw new RuntimeException("不整入力");
         }
-        System.out.println(a + " " + b);
+        System.out.printf("%d %d", a, b);
     }
 }
