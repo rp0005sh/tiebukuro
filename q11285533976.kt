4226a46c3aab09720a76bb598635f90d56b098d9
rp0005sh <37730329+rp0005sh@users.noreply.github.com>
2023-09-06 01:45:18 +0900

    Create q11285533976.kt

---
+++ q11285533976.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.CompoundButton.OnCheckedChangeListener
+import android.widget.Switch
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Switchにリスナー設定
+        val sw = findViewById<Switch>(R.id.switch1)
+        sw.setOnCheckedChangeListener(listener)
+
+        // 10秒後自動でスイッチをOFFにする
+        MainScope().launch {
+            delay(10000)
+            // Switchにリスナーを解除してから、スイッチOFF。その後、リスナー再設定
+            sw.apply {
+                setOnCheckedChangeListener(null)
+                isChecked = false
+                setOnCheckedChangeListener(listener)
+            }
+        }
+    }
+    /** スイッチの変化リスナーインスタンス */
+    private val listener = OnCheckedChangeListener { _, flg ->
+        Log.d("TAG", "flg=$flg")
+    }
+}
